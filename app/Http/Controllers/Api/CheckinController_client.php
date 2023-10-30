664421dddcc9a74ca2c50c8fcbb27c1f2f8415e6
Wongnarin Sukwichai <wongnarin.s@msu.ac.th>
2023-10-30 10:05:19 +0700

    update-client

---
+++ app/Http/Controllers/Api/CheckinController_client.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Carbon\Carbon;
+
+use App\Models\Checkin;
+use App\Models\Checkout;
+use App\Models\Member;
+use Illuminate\Support\Facades\DB;
+
+class CheckinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'uid' => 'required',
+            'capture' => 'required'
+        ]);
+
+        $now = Carbon::now();
+        $dat = $now->addYear(543)->format('Y-m-d');
+        $y = $now->format('Y');
+        $m = $now->format('m');
+        $d = $now->format('d');
+        $time = $now->format('H:i:s');
+
+        $res = Member::where('uid', $request['uid'])->first();
+
+        if (empty($res)) {
+
+            return response()->json([
+                'message' => 'ข้อมูลไม่ถูกต้อง. กรุณาตรวจสอบ'
+            ], 422);
+        } else {
+
+            $data = new Checkin();
+
+            $data->uid = $request['uid'];
+            $data->pic = $request['capture'];
+            $data->name = $res->name;
+            $data->surname = $res->surname;
+            $data->local = null;
+            $data->dat = $dat;
+            $data->d = $d;
+            $data->m = $m;
+            $data->y = $y;
+            $data->timein = $time;
+            $data->created = null;
+
+            $data->save();
+
+            DB::connection('mysql_server')->table('checkins')->insert([
+                'uid' => $request['uid'],
+                'pic' => $request['capture'],
+                'name' => $res->name,
+                'surname' => $res->surname,
+                'local' => 'arec',
+                'dat' => $dat,
+                'd' => $d,
+                'm' => $m,
+                'y' => $y,
+                'timein' => $time,
+                'created' => null
+            ]);
+        }
+
+        return response()->json($data);
+    }
+
+    public function checkout(Request $request)
+    {
+        $request->validate([
+            'uid' => 'required',
+            'capture' => 'required'
+        ]);
+
+        $now = Carbon::now();
+        $dat = $now->addYear(543)->format('Y-m-d');
+        $y = $now->format('Y');
+        $m = $now->format('m');
+        $d = $now->format('d');
+        $time = $now->format('H:i:s');
+
+        $res = Member::where('uid', $request['uid'])->first();
+
+        if (empty($res)) {
+
+            return response()->json([
+                'message' => 'ข้อมูลไม่ถูกต้อง. กรุณาตรวจสอบ'
+            ], 422);
+        } else {
+
+            $data = new Checkout();
+
+            $data->uid = $request['uid'];
+            $data->pic = $request['capture'];
+            $data->name = $res->name;
+            $data->surname = $res->surname;
+            $data->local = null;
+            $data->dat = $dat;
+            $data->d = $d;
+            $data->m = $m;
+            $data->y = $y;
+            $data->timeout = $time;
+            $data->created = null;
+
+            $data->save();
+
+            DB::connection('mysql_server')->table('checkouts')->insert([
+                'uid' => $request['uid'],
+                'pic' => $request['capture'],
+                'name' => $res->name,
+                'surname' => $res->surname,
+                'local' => 'arec',
+                'dat' => $dat,
+                'd' => $d,
+                'm' => $m,
+                'y' => $y,
+                'timeout' => $time,
+                'created' => null
+            ]);
+        }
+
+        return response()->json($data);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/welcome_client.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ระบบลงเวลาปฏิบัติงาน | AREC MSU</title>
+
+    <!-- Fonts -->
+    <link rel="icon" href="{{ url('img/favicon.jpg') }}">
+    <link href="https://fonts.googleapis.com/css?family=Prompt|Sarabun" rel="stylesheet">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="antialiased">
+
+    <div id="app"></div>
+
+    {{-- Server --}}
+    {{-- <div class="app-main__outer">
+        <div class="app-main__inner">
+            <div class="flex-center position-ref full-height">
+                <div class="message text-center" style="padding-top: 20%;">
+                    <a href="/login">
+                        <h5 class="text-gray-400 text-4xl hover:text-blue-400 cursor-pointer">Welcome to Worktime
+                            System</h5>
+                    </a><br>
+                    <h6 class="text-gray-300">Academic Resource Center MSU</h6>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+
+    {{-- Client --}}
+    <div id="clock" class="flex justify-center digital_clock_wrapper mt-8">
+        <div class="row rounded border-x-4 border-gray-300 px-8">
+            <div id="digit_clock_time" class="text-center text-xl"></div>
+            <div id="digit_clock_date"></div>
+        </div>
+    </div>
+
+    <div id="screenshots"></div>
+
+</body>
+
+<script type="text/javascript">
+
+    /** Clock **/
+    function currentTime() {
+        var date = new Date(); /* creating object of Date class */
+        var hour = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
+        hour = changeTime(hour);
+        min = changeTime(min);
+        sec = changeTime(sec);
+        document.getElementById("digit_clock_time").innerText = hour + " : " + min + " : " +
+            sec; /* adding time to the div */
+
+        var months = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม',
+            'กันยายน',
+            'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'
+        ];
+        var days = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์'];
+
+        var curWeekDay = days[date.getDay()]; // get day
+        var curDay = date.getDate(); // get date
+        var curMonth = months[date.getMonth()]; // get month
+        var curYear = date.getFullYear() + 543; // get year
+        var date = curWeekDay + ", " + curDay + " " + curMonth + " " + curYear; // get full date
+        document.getElementById("digit_clock_date").innerHTML = date;
+
+        var t = setTimeout(currentTime, 1000); /* setting timer */
+    }
+
+    function changeTime(k) {
+        /* appending 0 before time elements if less than 10 */
+        if (k < 10) {
+            return "0" + k;
+        } else {
+            return k;
+        }
+    }
+
+    currentTime();
+
+</script>
+
+</html>
